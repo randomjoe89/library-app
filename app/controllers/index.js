22752df88fd30797ec0e964c721a7ce4331007f2
David Ristovski <ristovski.david@gmail.com>
2019-04-27 15:14:51 -0400

    Adding a model.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,15 +3,21 @@ import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
-    emailAddress: '',
     headerTitle: 'Coming Soon',
+    responseMessage: '',
+    emailAddress: '',
 
     isValid: match('emailAddress',  /^.+@.+\..+$/),
     isDisabled: not('isValid'),
 
     actions: {
-        saveInvitation: function() {
-            alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+        saveInvitation() {
+            
+            const email = this.get('emailAddress');
+
+            const newInvitation = this.store.createRecord('invitation', {email: email});
+            newInvitation.save();
+
             this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
             this.set('emailAddress', '');
         }
